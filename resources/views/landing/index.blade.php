5254e68d8ff1de2e3a98d9874484050ea1503db3
aszraaurelita <aszra.tjahjaningrat23@student.uisi.ac.id>
2025-07-05 22:43:31 +0700

    update

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -559,5 +559,12 @@
         </div>
     </footer>
 </div>
+    <script>
+        document.getElementById("menu-toggle").addEventListener("click", function () {
+            const menu = document.getElementById("mobile-menu");
+            menu.classList.toggle("hidden");
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
