cd332e8bad14ce921a884577982031f4f7cedcf0
aszraaurelita <aszra.tjahjaningrat23@student.uisi.ac.id>
2025-07-05 22:08:31 +0700

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,9 +94,12 @@
 
             {{-- Landing Page --}}
             @if ($isLanding)
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                <!-- Isi menu yang akan dicollapse -->
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item"><a class="nav-link" href="#tentang">Tentang</a></li>
